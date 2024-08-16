86c53411d801ef421b5834125f14a248a032b301
Pop Lucas <lucaspop5511@gmail.com>
2024-08-17 00:05:29 +0300

    experimenting with different layouts and colors

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,18 +4,33 @@ import "./About.css";
 function About() {
     return (
         <section className="about-section">
-            <h2>About Me</h2>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis nostrum reprehenderit id laudantium. Eos dicta perferendis veniam alias at? Reprehenderit ab accusantium quas repellat. Incidunt assumenda sapiente repudiandae tenetur alias doloremque, itaque commodi ipsam necessitatibus sunt accusantium repellat deserunt nemo quae excepturi, quia quisquam nostrum! Corporis voluptates accusantium illo deserunt dolor. Numquam temporibus soluta tempore fugit. Porro, consequatur. Veniam, accusantium corporis provident architecto hic dolores quo aperiam qui nisi, consequatur vero nemo maxime? Quibusdam, quis expedita aut id quae deserunt similique nisi! Qui magni at ratione quisquam? Ea neque dignissimos soluta quod asperiores maiores dolores quae repudiandae dolore fugiat commodi sapiente minus ipsam quia eius, incidunt accusantium, nostrum consequatur numquam vitae atque unde. Eum vitae laudantium mollitia incidunt architecto vero harum officia exercitationem. Accusamus rem voluptates, aliquid temporibus ipsum suscipit molestias ea minus dicta nobis tenetur cumque voluptatibus sint consequatur dolorum est sequi mollitia necessitatibus fugiat? Nulla assumenda molestiae aspernatur rerum rem expedita quidem consequuntur, modi incidunt fugiat, nobis facere debitis reprehenderit quos amet ut quasi ea unde. Blanditiis ipsa hic autem reiciendis explicabo modi, eum officiis, minima error voluptas rem neque facilis numquam officia? Totam odit ipsam doloremque quisquam, modi ducimus obcaecati, praesentium laboriosam quidem distinctio minus nemo consectetur.
-            </p>
-            <h3>Skills</h3>
-            <ul>
-                <li>HTML, CSS, JavaScript</li>
-                <li>React.js</li>
-                <li>Node.js</li>
-                <li>Git & GitHub</li>
-            </ul>
-            <div className="extension pixel-corner"></div>
+
+            <div class="about-me">
+                <h2>About Me</h2>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis nostrum reprehenderit id laudantium. Eos dicta perferendis veniam alias at? Reprehenderit ab accusantium quas repellat. Incidunt assumenda sapiente repudiandae tenetur alias doloremque, itaque commodi ipsam necessitatibus sunt accusantium repellat deserunt nemo quae excepturi, quia quisquam nostrum! Corporis voluptates accusantium illo deserunt dolor. Numquam temporibus soluta tempore fugit. Porro, consequatur. Veniam, accusantium corporis provident architecto hic dolores quo aperiam qui nisi, consequatur vero nemo maxime? Quibusdam, quis expedita aut id quae deserunt similique nisi! Qui magni at ratione quisquam? Ea neque dignissimos soluta quod asperiores maiores dolores quae repudiandae dolore fugiat commodi sapiente minus ipsam quia eius, incidunt accusantium, nostrum consequatur numquam vitae atque unde. Eum vitae laudantium mollitia incidunt architecto vero harum officia exercitationem. Accusamus rem voluptates, aliquid temporibus ipsum suscipit molestias ea minus dicta nobis tenetur cumque voluptatibus sint consequatur dolorum est sequi mollitia necessitatibus fugiat? Nulla assumenda molestiae aspernatur rerum rem expedita quidem consequuntur, modi incidunt fugiat, nobis facere debitis reprehenderit quos amet ut quasi ea unde. Blanditiis ipsa hic autem reiciendis explicabo modi, eum officiis, minima error voluptas rem neque facilis numquam officia? Totam odit ipsam doloremque quisquam, modi ducimus obcaecati, praesentium laboriosam quidem distinctio minus nemo consectetur.
+                </p>
+            </div>
+
+            <div class="skills">
+                <h3>Skills</h3>
+                <ul>
+                    <li>HTML, CSS, JavaScript</li>
+                    <li>React.js</li>
+                    <li>Node.js</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                    <li>Git & GitHub</li>
+                </ul>
+                <div className="extension pixel-corner"></div>
+            </div>
+
         </section>
     );
 }
